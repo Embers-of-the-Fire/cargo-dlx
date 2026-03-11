7afca7d1098ce6960a281037e5adc01737a6877a
Ed Page <eopage@gmail.com>
2026-03-11 12:09:37 -0500

    test: Fix test structure

--- tests/testsuite/main.rs
+++ tests/testsuite/main.rs
@@ -1,3 +1,49 @@
-mod dlx;
+mod cargo_dlx;
 
-pub use dlx::*;
+use cargo_test_support::{ArgLineCommandExt, Execs, Project, TestEnvCommandExt, execs, process};
+use cargo_util::ProcessBuilder;
+
+pub fn cargo_dlx_exe() -> std::path::PathBuf {
+    snapbox::cmd::cargo_bin!("cargo-dlx").to_path_buf()
+}
+
+pub trait ProjectExt {
+    /// Creates an `Execs` instance to run the `cargo-dlx` binary
+    fn cargo_dlx(&self, cmd: &str) -> Execs;
+    /// Creates an `Execs` instance to run the globally installed `cargo` command
+    fn cargo_global(&self, cmd: &str) -> Execs;
+}
+
+impl ProjectExt for Project {
+    fn cargo_dlx(&self, cmd: &str) -> Execs {
+        let cargo_dlx = cargo_dlx_exe();
+
+        let mut p = process(&cargo_dlx);
+        p.cwd(self.root()).arg_line(cmd);
+
+        execs().with_process_builder(p)
+    }
+
+    fn cargo_global(&self, cmd: &str) -> Execs {
+        let cargo = std::env::var_os("CARGO").unwrap_or("cargo".into());
+
+        let mut p = ProcessBuilder::new(cargo);
+        p.test_env().cwd(self.root()).arg_line(cmd);
+
+        execs().with_process_builder(p)
+    }
+}
+
+pub trait CargoCommandExt {
+    fn cargo_ui() -> Self;
+}
+
+impl CargoCommandExt for snapbox::cmd::Command {
+    fn cargo_ui() -> Self {
+        Self::new(cargo_dlx_exe())
+            .with_assert(cargo_test_support::compare::assert_ui())
+            .env("CARGO_TERM_COLOR", "always")
+            .env("CARGO_TERM_HYPERLINKS", "true")
+            .test_env()
+    }
+}
